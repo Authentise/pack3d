0c81879ac2549ad245bb7323a613616b595099f0
Ted Bennett <ted@authentise.com>
2024-09-12 09:23:19 -0400

    chore: test master ricoh copacking

--- pack3d/pack3d_test.go
+++ pack3d/pack3d_test.go
@@ -7,8 +7,25 @@ import (
 	"github.com/Authentise/pack3d/pack3d"
 )
 
-func TestPack(t *testing.T) {
+func TestCoPack(t *testing.T) {
 	config, err := pack3d.ParseConfig("../tests/fixtures/copack.json")
+
+	fmt.Printf("%v", config)
+	if err != nil {
+		t.Fatalf("Failed to load config: %s", err)
+	}
+
+	_, err = pack3d.Pack(config)
+
+	if err != nil {
+		t.Fatalf("Failed to pack model: %s", err)
+	}
+}
+
+// Tests an input file with old master-ricoh api
+// These input files don't have axes_lock and spacing fields
+func TestMasterRicohCoPack(t *testing.T) {
+	config, err := pack3d.ParseConfig("../tests/fixtures/master-ricoh-copack.json")
 
 	fmt.Printf("%v", config)
 	if err != nil {
