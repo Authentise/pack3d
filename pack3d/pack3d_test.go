9e4623f0dbee90f911b4ff70e9234f2b6c00bb9a
Ted Bennett <ted@authentise.com>
2024-09-24 12:00:29 -0400

    test: remaining manual tests

--- pack3d/pack3d_test.go
+++ pack3d/pack3d_test.go
@@ -24,6 +24,25 @@ func testPackingInputFile(t *testing.T, input string) {
 	}
 }
 
+// Pass in the expected number of items packed
+func testPartialPackingInputFile(t *testing.T, input string, expectedPacking int) {
+	config, err := pack3d.ParseConfig(input)
+
+	if err != nil {
+		t.Fatalf("Failed to load config: %s", err)
+	}
+
+	output, err := pack3d.Pack(config)
+
+	if err != nil {
+		t.Fatalf("Failed to pack config: %s", err)
+	}
+
+	if len(output.Model.Items) != expectedPacking {
+		t.Fatalf("Failed to pack all models: packed %d of %d", len(output.Model.Items), expectedPacking)
+	}
+}
+
 func TestCoPack(t *testing.T) {
 	testPackingInputFile(t, "../tests/fixtures/copack.json")
 }
@@ -35,20 +54,31 @@ func TestMasterRicohCoPack(t *testing.T) {
 }
 
 func TestBuildPlateTooSmall(t *testing.T) {
-	// This test has been removed since it takes so long.
-	// Pack3d's algorithm attempts to pack using binary search to find
-	// the optimal number of items. Each step takes at least 10s, and
-	// so failing packs take a long time to run.
+	// NOTE: This test fails in master-ricoh but NOT in dev, and doesn't fail here.
 	t.Skip()
+	testPartialPackingInputFile(t, "../tests/fixtures/too-small.json", 1)
+}
 
-	testPackingInputFile(t, "../tests/fixtures/too-small.json")
+func TestPartialPack(t *testing.T) {
+	// NOTE: This test fails in master-ricoh but NOT in dev, and doesn't fail here.
+	t.Skip()
+	testPartialPackingInputFile(t, "../tests/fixtures/partial-pack.json", 2) // Currently 3
+}
+
+func TestSc45665(t *testing.T) {
+	// NOTE: This test fails in master-ricoh but NOT in dev, and doesn't fail here.
+	t.Skip()
+	testPartialPackingInputFile(t, "../tests/fixtures/sc45665.json", 9) // Currently 10
 }
 
 func TestCh32838(t *testing.T) {
-
 	testPackingInputFile(t, "../tests/fixtures/ch32838.json")
 }
 
 func TestSc46802(t *testing.T) {
 	testPackingInputFile(t, "../tests/fixtures/sc46802.json")
+}
+
+func TestSc44515(t *testing.T) {
+	testPackingInputFile(t, "../tests/fixtures/sc44515.json")
 }
